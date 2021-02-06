1ab7f5212bc6ffe1c7c90fb74a3dadc9afffafeb
Wharkk <76532812+Wharkk@users.noreply.github.com>
2021-02-06 13:58:42 +0100

    acceuil.php

---
+++ acceuil.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Nuclearium-MCPE</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="carousel.css" rel="stylesheet">
+<link href="styles.css" rel="stylesheet">
+</head>
+<?php include('header.php'); ?>
+
+
+<body>
+
+
+
+
+</body>
+<script src="js/bootstrap.bundle.min.js"></script>
+</html>
+
+
